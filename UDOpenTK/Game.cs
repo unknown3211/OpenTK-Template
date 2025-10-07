5a87fb363ed508fc3a68075aac0ef5fdbc312b86
unknown3211 <100625853+unknown3211@users.noreply.github.com>
2025-10-07 10:59:18 -0400

    Files Added

---
+++ UDOpenTK/Game.cs
@@ -0,0 +1,59 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace UDOpenTK
+{ 
+    internal class Game : GameWindow
+    {
+        float[] vertices =
+        {
+            0f, 0.5f, 0f, // top
+           -0.5f, -0.5f, 0f, // bottom left
+           0.5f, -0.5f, 0f // bottom right
+        };
+
+        int width;
+        int height;
+
+        private Mesh mesh = new Mesh();
+        public Game(int width, int height) : base(GameWindowSettings.Default, NativeWindowSettings.Default)
+        {
+            this.width = width;
+            this.height = height;
+            CenterWindow(new Vector2i(width, height));
+        }
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0, 0, e.Width, e.Height);
+            this.width = e.Width;
+            this.height = e.Height;
+        }
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            mesh.Load(vertices, "default.vert", "default.frag");
+        }
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+            mesh.Shutdown();
+        }
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.ClearColor(0f, 0f, 0f, 1f);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            mesh.Draw();
+
+            Context.SwapBuffers();
+            base.OnRenderFrame(args);
+        }
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+    }
+}
---
+++ UDOpenTK/Mesh.cs
@@ -0,0 +1,83 @@
+﻿using OpenTK.Graphics.OpenGL4;
+
+namespace UDOpenTK
+{
+    internal class Mesh
+    {
+        int vao;
+        int vbo;
+        int shaderProgram;
+
+        public void Load(float[] vertices, string vertexShaderPath, string fragmentShaderPath)
+        {
+            vao = GL.GenVertexArray();
+            vbo = GL.GenBuffer();
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            GL.BindVertexArray(vao);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
+            GL.EnableVertexAttribArray(0);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+
+            shaderProgram = GL.CreateProgram();
+
+            int vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShader, LoadShader(vertexShaderPath));
+            GL.CompileShader(vertexShader);
+
+            int fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentShader, LoadShader(fragmentShaderPath));
+            GL.CompileShader(fragmentShader);
+
+            GL.AttachShader(shaderProgram, vertexShader);
+            GL.AttachShader(shaderProgram, fragmentShader);
+
+            GL.LinkProgram(shaderProgram);
+
+            GL.GetProgram(shaderProgram, GetProgramParameterName.LinkStatus, out int success);
+            if (success == 0)
+            {
+                string infoLog = GL.GetProgramInfoLog(shaderProgram);
+                Console.WriteLine($"Shader Program Linking Error: {infoLog}");
+            }
+
+            GL.DeleteShader(vertexShader);
+            GL.DeleteShader(fragmentShader);
+        }
+
+        public void Draw()
+        {
+            GL.UseProgram(shaderProgram);
+            GL.BindVertexArray(vao);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+        }
+
+        public void Shutdown()
+        {
+            GL.DeleteVertexArray(vao);
+            GL.DeleteBuffer(vbo);
+            GL.DeleteProgram(shaderProgram);
+        }
+
+        public static string LoadShader(string filePath)
+        {
+            string shaderSource = "";
+            try
+            {
+                using (StreamReader reader = new StreamReader("../../../shaders/" + filePath))
+                {
+                    shaderSource = reader.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed To Load Shader: " + e.Message);
+            }
+            return shaderSource;
+        }
+    }
+}
